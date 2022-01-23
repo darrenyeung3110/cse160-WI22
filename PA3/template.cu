2d4eaac2f8ee82da71f1c3234eadd647d36f0897
dyeung <dyeung@dyeung-24276.pods.dyeung.svc.cluster.local>
2022-01-23 15:43:55 -0800

    Finished pa3

--- PA3/template.cu
+++ PA3/template.cu
@@ -11,12 +11,30 @@
     }                                                                     \
   } while (0)
 
-// Compute C = A * B
+#define BLOCK_WIDTH 2
+// block size 2x2
+// Compute C = A * Bx 
 __global__ void matrixMultiply(float *A, float *B, float *C, int numARows,
                                int numAColumns, int numBRows,
                                int numBColumns, int numCRows,
                                int numCColumns) {
   //@@ Insert code to implement matrix multiplication here
+    int row = (blockIdx.x * BLOCK_WIDTH) + threadIdx.x; 
+    int col  = (blockIdx.y * BLOCK_WIDTH) + threadIdx.y; 
+    if (row >= 0 && row < numCRows && col >= 0  && col < numCColumns) {
+      // printf("%d lol %d\n", row, col);
+      int aPtr = row * numAColumns; 
+      int bPtr = col; 
+      float sum = 0.0; 
+      for (int i = 0; i < numAColumns; i++) {
+          float currentMultiply = A[aPtr] * B[bPtr]; 
+          sum += currentMultiply; 
+          aPtr++; 
+          bPtr += numBColumns; // ?
+      }
+      int collapsedOutputIndex = (row * numCColumns) + col;
+      C[collapsedOutputIndex] = sum; 
+    }
 }
 
 int main(int argc, char **argv) {
@@ -34,8 +52,6 @@ int main(int argc, char **argv) {
   int numCRows;    // number of rows in the matrix C (you have to set this)
   int numCColumns; // number of columns in the matrix C (you have to set
                    // this)
-  int aSize = numARows * numAColumns; 
-  int bSize = numBRows * numBColumns; 
 
 
   args = gpuTKArg_read(argc, argv);
@@ -45,12 +61,15 @@ int main(int argc, char **argv) {
                             &numAColumns);
   hostB = (float *)gpuTKImport(gpuTKArg_getInputFile(args, 1), &numBRows,
                             &numBColumns);
+  int aSize = numARows * numAColumns; 
+  int bSize = numBRows * numBColumns; 
   //@@ Set numCRows and numCColumns
   // ARowsXACols * BRowsXBCols = ARows X BCols
   numCRows    = numARows; 
   numCColumns = numBColumns; 
   int cSize = numCRows * numCColumns;
   //@@ Allocate the hostC matrix
+  hostC = (float*)malloc(cSize * sizeof(float));
   gpuTKTime_stop(Generic, "Importing data and creating memory on host");
 
   gpuTKLog(TRACE, "The dimensions of A are ", numARows, " x ", numAColumns);
@@ -70,21 +89,33 @@ int main(int argc, char **argv) {
   gpuTKTime_stop(GPU, "Copying input memory to the GPU.");
 
   //@@ Initialize the grid and block dimensions here
+  int gridRows;
+  int gridCols; 
+  gridRows = ceil((float)numCRows/BLOCK_WIDTH); 
+  gridCols = ceil((float)numCColumns/BLOCK_WIDTH); 
+
+  dim3 grid_size(gridRows, gridCols); 
+  dim3 block_size(BLOCK_WIDTH, BLOCK_WIDTH); // 2x2
 
   gpuTKTime_start(Compute, "Performing CUDA computation");
   //@@ Launch the GPU Kernel here
-
+  matrixMultiply<<<grid_size, block_size>>>(deviceA, deviceB, deviceC, numARows, numAColumns, numBRows, numBColumns, 
+  numCRows, numCColumns);
   cudaDeviceSynchronize();
   gpuTKTime_stop(Compute, "Performing CUDA computation");
 
   gpuTKTime_start(Copy, "Copying output memory to the CPU");
   //@@ Copy the GPU memory back to the CPU here
-
+  cudaMemcpy(hostC, deviceC, cSize *(sizeof(float)), cudaMemcpyDeviceToHost); 
+  // printf("%d\n", deviceC[0]);
+  // printf("%d\n", hostC[0]);
   gpuTKTime_stop(Copy, "Copying output memory to the CPU");
 
   gpuTKTime_start(GPU, "Freeing GPU Memory");
   //@@ Free the GPU memory here
-
+  cudaFree(deviceA); 
+  cudaFree(deviceB); 
+  cudaFree(deviceC); 
   gpuTKTime_stop(GPU, "Freeing GPU Memory");
 
   gpuTKSolution(args, hostC, numCRows, numCColumns);
